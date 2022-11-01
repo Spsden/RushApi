8c70952b72929062b401da5830bd37dec4e3dffa
Spsden <pratapsinghsuraj420@gmail.com>
2022-11-01 20:32:51 +0530

    added more results

--- utilities/searchpure.py
+++ utilities/searchpure.py
@@ -26,6 +26,17 @@ class SearchPure:
 
             }
             search_result.append(app_details)
+        
+        for result in otherResults:
+            app_details = {
+                "name" : result.find('p').text,
+                "icon" : result.find('img')['src'],
+                "dpage" : result.find('a',class_='more-down')['href'],
+                "dev" : result.find('p',class_='developer').find_next().text,
+                "span" : "NA",
+                "source" : 'apkpure'
+            }
+            search_result.append(app_details)
         return search_result
         
 
